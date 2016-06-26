10def4bcec7f3a02a422213a37434d5fcbf8d1b5
Trevor Smith <tsmith@oconics.com>
2016-06-26 17:00:14 +0930

    Added new API Sandbox controller

---
+++ src/TheWorld/Controllers/api/SandBox.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers.api
+{
+    public class SandBox : Controller
+    {
+        [HttpGet("api/sandbox")]
+        public JsonResult Get()
+        {
+            return Json(new { Message = "This is a test api method" });
+        }
+    }
+}
