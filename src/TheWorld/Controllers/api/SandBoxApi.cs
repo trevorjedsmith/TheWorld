7276185daa6424f91c59c42211912e85d8c895cf
Trevor Smith <tsmith@oconics.com>
2016-06-26 17:24:22 +0930

    Added SandBoxApi

---
+++ src/TheWorld/Controllers/api/SandBoxApi.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.Controllers.api
+{
+    public class SandBoxApi : Controller
+    {
+        [HttpGet]
+        public JsonResult Get()
+        {
+            return Json(new { Message = "Successful Get()" });
+        }
+    }
+}
